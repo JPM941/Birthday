45f77c671f8bc3c66ccf38fdb41b1b532c4fbd83
Julien Martin <julien.martin94@icloud.com>
2023-05-11 15:59:55 +0200

    information added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "users#index"
+  get :informations, to: "pages#informations"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
