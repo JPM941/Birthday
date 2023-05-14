1f8834d18a94ca042ac26daadc6ef828a567a2d4
Julien Martin <julien.martin94@icloud.com>
2023-05-14 20:45:41 +0200

    form done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path
+      raise
+      redirect_to howmany_path
     else
       render :new, status: :unprocessable_entity
     end
